b83a1b63bd8a8b7bd4bfd7abeee95eb6353eee2e
Hossam Hassan <111233969+Hossamfc9@users.noreply.github.com>
2025-01-23 13:21:00 +0200

    [1267] [Time Beats: 100.00%] [Memory Beats: 39.90%] - LeetPush

--- DCP-01-25/1267-Count-Servers-that-Communicate.cpp
+++ DCP-01-25/1267-Count-Servers-that-Communicate.cpp
@@ -1,31 +1,8 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<bool>> vis;
-    vector<vector<int>> grid;
-    int dx[4] = {0, 0, 1, -1};
-    int dy[4] = {1, -1, 0, 0};
-
-    bool valid(int i, int j) {
-        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == 1;
-    }
-
-    int dfs(int i, int j) {
-        int cnt = 1;
-        vis[i][j] = 1;
-        for(int k = 0; k < 4; k++) {
-            int newi = i + dx[k];
-            int newj = j + dy[k];
-            if(valid(newi, newj) && !vis[newi][newj]) {
-                cnt += dfs(newi, newj);
-            }
-        }
-        return cnt;
-    }
     int countServers(vector<vector<int>>& grid) {
-        this->grid = grid;
         n = grid.size(), m = grid[0].size();
-        vis = vector<vector<bool>>(n, vector<bool> (m));
         int ans = 0;
         vector<int> row(n);
         for(int i = 0; i < n; i++) {
